a9c97de6d206897f4acee1e21e6ccd74401fede1
Siddharth Thakkar <sid2011thakkar@gmail.com>
2025-01-14 20:34:20 -0500

    tried-frontend
    dysfunctional/semi-functional frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import {FullSignup} from "./components/FullSignup"
+import {FullSignin} from "./components/FullSignin"
 
 function App() {
 
   return (
     <div>
-        Hello world
+                 <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<FullSignup />} />
+          <Route path="/signin" element={<FullSignin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
